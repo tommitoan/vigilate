b91b5f1b682ce6deb3f51e024606e1edfb397a35
tommitoan <htm1995@gmail.com>
2024-05-24 22:12:49 +0700

    monitoring additional services: https, ssl

--- internal/repository/dbrepo/host_postgresql.go
+++ internal/repository/dbrepo/host_postgresql.go
@@ -36,13 +36,31 @@ func (m *postgresDBRepo) InsertHost(h models.Host) (int, error) {
 	}
 
 	// add host services and set to inactive
-	stmt := `
-		insert into host_services (host_id, service_id, active, schedule_number, schedule_unit,
-		status, created_at, updated_at) values ($1, 1, 0, 3, 'm', 'pending', $2, $3)`
-
-	_, err = m.DB.ExecContext(ctx, stmt, newID, time.Now(), time.Now())
+	query = `select id from services`
+	serviceRows, err := m.DB.QueryContext(ctx, query)
 	if err != nil {
-		return newID, err
+		log.Println(err)
+		return 0, err
+	}
+	defer serviceRows.Close()
+
+	for serviceRows.Next() {
+		var svcID int
+		err := serviceRows.Scan(&svcID)
+		if err != nil {
+			log.Println(err)
+			return 0, err
+		}
+
+		stmt := `
+			insert into host_services 
+		    	(host_id, service_id, active, schedule_number, schedule_unit,
+				status, created_at, updated_at) values ($1, $2, 0, 3, 'm', 'pending', $3, $4)`
+
+		_, err = m.DB.ExecContext(ctx, stmt, newID, svcID, time.Now(), time.Now())
+		if err != nil {
+			return newID, err
+		}
 	}
 
 	return newID, nil
@@ -86,12 +104,13 @@ func (m *postgresDBRepo) GetHostByID(id int) (models.Host, error) {
 			select 
 				hs.id, hs.host_id, hs.service_id, hs.active, hs.schedule_number, hs.schedule_unit, 
 				hs.last_check, hs.status, hs.created_at, hs.updated_at,
-				s.id, s.service_name, s.active, s.icon, s.created_at, s.updated_at
+				s.id, s.service_name, s.active, s.icon, s.created_at, s.updated_at, hs.last_message
 			from 
 				host_services hs
 				left join services s on (s.id = hs.service_id)
 			where
-				host_id = $1`
+				host_id = $1
+			order by s.service_name`
 
 	rows, err := m.DB.QueryContext(ctx, query, h.ID)
 	if err != nil {
@@ -120,6 +139,7 @@ func (m *postgresDBRepo) GetHostByID(id int) (models.Host, error) {
 			&hs.Service.Icon,
 			&hs.Service.CreatedAt,
 			&hs.Service.UpdatedAt,
+			&hs.LastMessage,
 		)
 		if err != nil {
 			log.Println(err)
